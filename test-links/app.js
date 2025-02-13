223649cd3d6d5ca9506705b4c9d27b9085bb57c1
Mike Bennett <94943459+mvbennett@users.noreply.github.com>
2025-02-13 16:14:39 -0500

    feat: added question interface

--- test-links/app.js
+++ test-links/app.js
@@ -2,8 +2,8 @@ const fs = require("fs");
 const readline = require("readline");
 const path = require("path");
 
-const project = process.argv?.[2];
-const environment = process.argv?.[3];
+const project = process.argv?.[2]?.trim();
+const environment = process.argv?.[3]?.trim();
 
 function getData() {
   const dataFile = path.resolve(__dirname, "test-links.json");
@@ -15,52 +15,40 @@ function getData() {
   return data;
 }
 
+function getLinks(project, environment) {
+  if (!project?.length) return [];
+
+  const urls = environment?.length
+    ? data[project].map((link) => data.envs[project][environment] + link)
+    : data[project];
+  return urls;
+}
+
+function printLinks(urls) {
+  if (!urls?.length) return;
+
+  urls.forEach((url) => console.log(url));
+}
+
 const data = getData();
 
-if (project?.length && environment?.length) {
-  const urls = data[project].map(
-    (link) => data.envs[project][environment] + link
-  );
-  urls.forEach((link) => {
-    console.log(link);
-  });
-} else if (project?.length) {
-  const urls = data[project];
-  urls.forEach((link) => {
-    console.log(link);
-  });
+if (project?.length) {
+  const urls = getLinks(project, environment);
+  printLinks(urls);
 } else {
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
   });
 
-  const inputProject = rl.question("Enter project name: ", (input) => {
-    if (!input?.length) rl.close;
+  rl.question("Enter project name: ", (projectInput) => {
+    if (!projectInput?.length) return rl.close();
 
-    return input;
+    rl.question("Enter env: ", (envInput) => {
+      const urls = getLinks(projectInput.trim(), envInput.trim());
+
+      printLinks(urls);
+      rl.close();
+    });
   });
-
-  const inputEnv = rl.question(
-    "Enter the environment or hit enter to get paths",
-    (input) => {
-      return input;
-    }
-  );
-
-  if (!inputEnv?.length) {
-    const urls = data[inputProject];
-    urls.forEach((link) => {
-      console.log(link);
-    });
-    rl.close();
-  } else {
-    const urls = data[inputProject].map(
-      (link) => data.envs[inputProject][inputEnv] + link
-    );
-    urls.forEach((link) => {
-      console.log(link);
-    });
-    rl.close();
-  }
 }
